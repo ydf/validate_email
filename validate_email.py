bbe20d45d672718910d8ae463209e3a461e80120
admin <you@example.com>
2017-05-10 18:52:48 +0800

    fix checkemail

--- validate_email.py
+++ validate_email.py
@@ -16,27 +16,16 @@
 # This implementation aims to be faithful to the RFC, with the
 # exception of a circular definition (see comments below), and
 # with the omission of the pattern components marked as "obsolete".
+from gevent import monkey; monkey.patch_all()
 
 import re
 import smtplib
 import logging
 import socket
 
-try:
-    raw_input
-except NameError:
-    def raw_input(prompt=''):
-        return input(prompt)
-
-try:
-    import DNS
-    ServerError = DNS.ServerError
-    DNS.DiscoverNameServers()
-except (ImportError, AttributeError):
-    DNS = None
-
-    class ServerError(Exception):
-        pass
+import DNS
+from DNS import ServerError
+DNS.DiscoverNameServers()
 
 # All we are really doing is comparing the input string to one
 # gigantic regular expression.  But building that regexp, and
@@ -105,11 +94,10 @@ def get_mx_ip(hostname):
                 MX_DNS_CACHE[hostname] = None
             else:
                 raise
-
     return MX_DNS_CACHE[hostname]
 
 
-def validate_email(email, check_mx=False, verify=False, debug=False, smtp_timeout=10):
+def validate_email(email, debug=False, smtp_timeout=10):
     """Indicate whether the given string is a valid email address
     according to the 'addr-spec' portion of RFC 2822 (see section
     3.4.1).  Parts of the spec that are marked obsolete are *not*
@@ -124,89 +112,48 @@ def validate_email(email, check_mx=False, verify=False, debug=False, smtp_timeou
         logger = None
 
     try:
-        assert re.match(VALID_ADDRESS_REGEXP, email) is not None
-        check_mx |= verify
-        if check_mx:
-            if not DNS:
-                raise Exception('For check the mx records or check if the email exists you must '
-                                'have installed pyDNS python package')
-            hostname = email[email.find('@') + 1:]
-            mx_hosts = get_mx_ip(hostname)
-            if mx_hosts is None:
-                return False
-            for mx in mx_hosts:
-                try:
-                    if not verify and mx[1] in MX_CHECK_CACHE:
-                        return MX_CHECK_CACHE[mx[1]]
-                    smtp = smtplib.SMTP(timeout=smtp_timeout)
-                    smtp.connect(mx[1])
-                    MX_CHECK_CACHE[mx[1]] = True
-                    if not verify:
-                        try:
-                            smtp.quit()
-                        except smtplib.SMTPServerDisconnected:
-                            pass
-                        return True
-                    status, _ = smtp.helo()
-                    if status != 250:
-                        smtp.quit()
-                        if debug:
-                            logger.debug(u'%s answer: %s - %s', mx[1], status, _)
-                        continue
-                    smtp.mail('')
-                    status, _ = smtp.rcpt(email)
-                    if status == 250:
-                        smtp.quit()
-                        return True
+        hostname = email[email.find('@') + 1:]
+        mx_hosts = get_mx_ip(hostname)
+        if mx_hosts is None:
+            return False
+        for mx in mx_hosts:
+            try:
+                smtp = smtplib.SMTP(timeout=smtp_timeout)
+                mx_host = mx[1]
+                smtp.connect(mx_host)
+                status, _ = smtp.helo(name="mail.qq.com")
+                if status != 250:
+                    smtp.quit()
                     if debug:
                         logger.debug(u'%s answer: %s - %s', mx[1], status, _)
-                    smtp.quit()
-                except smtplib.SMTPServerDisconnected:  # Server not permits verify user
-                    if debug:
-                        logger.debug(u'%s disconected.', mx[1])
-                except smtplib.SMTPConnectError:
-                    if debug:
-                        logger.debug(u'Unable to connect to %s.', mx[1])
-            return None
-    except AssertionError:
-        return False
+                    return False
+                smtp.mail('')
+                status, _ = smtp.rcpt(email)
+                if debug:
+                    logger.debug(u'%s answer: %s - %s', mx[1], status, _)
+                if status != 250:
+                    return False
+                smtp.quit()
+            except smtplib.SMTPServerDisconnected:  # Server not permits verify user
+                if debug:
+                    logger.debug(u'%s disconected.', mx[1])
+            except smtplib.SMTPConnectError:
+                if debug:
+                    logger.debug(u'Unable to connect to %s.', mx[1])
+        return True
     except (ServerError, socket.error) as e:
         if debug:
             logger.debug('ServerError or socket.error exception raised (%s).', e)
         return None
-    return True
+
 
 if __name__ == "__main__":
-    import time
-    while True:
-        email = raw_input('Enter email for validation: ')
-
-        mx = raw_input('Validate MX record? [yN] ')
-        if mx.strip().lower() == 'y':
-            mx = True
-        else:
-            mx = False
-
-        validate = raw_input('Try to contact server for address validation? [yN] ')
-        if validate.strip().lower() == 'y':
-            validate = True
-        else:
-            validate = False
-
         logging.basicConfig()
-
-        result = validate_email(email, mx, validate, debug=True, smtp_timeout=1)
+        email = "10000@qq.com"
+        result = validate_email(email, debug=True, smtp_timeout=1)
         if result:
-            print("Valid!")
+            print("Valid!", email)
         elif result is None:
             print("I'm not sure.")
         else:
             print("Invalid!")
-
-        time.sleep(1)
-
-
-# import sys
-
-# sys.modules[__name__],sys.modules['validate_email_module'] = validate_email,sys.modules[__name__]
-# from validate_email_module import *
